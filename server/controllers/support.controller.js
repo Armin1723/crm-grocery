58d9c123c7e041cb62486dc266f6e91c901f6c2b
Uzair Alam <a-8278@kmclu.ac.in>
2025-02-17 18:12:30 +0530

    add: company details in client fetching

--- server/controllers/support.controller.js
+++ server/controllers/support.controller.js
@@ -3,131 +3,126 @@ const User = require("../models/user.model");
 const { loadSubscriptionsToCache } = require("../helpers/subscriptionCache");
 
 const getClients = async (req, res) => {
-    const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10 } = req.query;
 
-    const clients = await User.find({ role: "admin" }).limit(limit).skip((page - 1) * limit);
-
-    const totalResults = await User.countDocuments({ role: "admin" });
-
-    const totalPages = Math.ceil(totalResults / limit);
-
-    res.status(200).json({
-        success: true,
-        clients,
-        totalResults,
-        totalPages,
+  const clients = await User.find({ role: "admin" })
+    .populate("company", "subscription name phone")
+    .limit(limit)
+    .skip((page - 1) * limit)
+    .sort({
+      [sort]: sortType,
     });
+
+  const totalResults = await User.countDocuments({ role: "admin" });
+
+  const totalPages = Math.ceil(totalResults / limit);
+
+  res.status(200).json({
+    success: true,
+    clients,
+    totalResults,
+    totalPages,
+  });
 };
 
 const getClientById = async (req, res) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const client = await User.findById(id).populate("company");
 
-    const client = await User.findById(id).populate("company");
-
-    if (!client) {
-        return res.status(404).json({
-            success: false,
-            message: "Client not found",
-        });
-    };
-
-    res.status(200).json({
-        success: true,
-        client,
+  if (!client) {
+    return res.status(404).json({
+      success: false,
+      message: "Client not found",
     });
+  }
 
+  res.status(200).json({
+    success: true,
+    client,
+  });
 };
 
 const getLeadsCompany = async (req, res) => {
-    const { phone } = req.params;
+  const { phone } = req.params;
 
-    const company = await Company.findOne({ phone });
+  const company = await Company.findOne({ phone });
 
-    if (!company) {
-        return res.status(404).json({
-            success: false,
-            message: "Company not found",
-            company: {},
-        });
-    }
-
-    res.status(200).json({
-        success: true,
-        company,
+  if (!company) {
+    return res.status(404).json({
+      success: false,
+      message: "Company not found",
+      company: {},
     });
+  }
+
+  res.status(200).json({
+    success: true,
+    company,
+  });
 };
 
 const activateClient = async (req, res) => {
-    
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const {subscriptionEndDate, subscription} = req.body;
+  const { subscriptionEndDate, subscription } = req.body;
 
-    const clientCompany = await Company.findOne({
-        admin: id,
+  const clientCompany = await Company.findOne({
+    admin: id,
+  });
+
+  if (!clientCompany) {
+    return res.status(404).json({
+      success: false,
+      message: "Client not found",
     });
+  }
 
-    if (!clientCompany) {
+  // Activate client
+  clientCompany.subscription = subscription;
+  clientCompany.subscriptionStartDate = new Date();
+  clientCompany.subscriptionEndDate = new Date(subscriptionEndDate);
+  await clientCompany.save();
 
-        return res.status(404).json({
-            success: false,
-            message: "Client not found",
-        });
+  // Reload subscriptions cache
+  loadSubscriptionsToCache();
 
-    }
-
-    // Activate client
-    clientCompany.subscription = subscription;
-    clientCompany.subscriptionStartDate = new Date();
-    clientCompany.subscriptionEndDate = new Date(subscriptionEndDate);
-    await clientCompany.save();
-
-    // Reload subscriptions cache
-    loadSubscriptionsToCache(); 
-
-    res.status(200).json({
-        success: true,
-        message: "Client activated",
-    });
-
+  res.status(200).json({
+    success: true,
+    message: "Client activated",
+  });
 };
 
 const deactivateClient = async (req, res) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const clientCompany = await Company.findOne({
+    admin: id,
+  });
 
-    const clientCompany = await Company.findOne({
-        admin: id,
+  if (!clientCompany) {
+    return res.status(404).json({
+      success: false,
+      message: "Client not found",
     });
+  }
 
-    if (!clientCompany) {
+  // Deactivate client
+  clientCompany.subscription = null;
+  clientCompany.subscriptionStartDate = null;
+  clientCompany.subscriptionEndDate = null;
+  await clientCompany.save();
 
-        return res.status(404).json({
-            success: false,
-            message: "Client not found",
-        });
-
-    }
-
-    // Deactivate client
-    clientCompany.subscription = null;
-    clientCompany.subscriptionStartDate = null;
-    clientCompany.subscriptionEndDate = null;
-    await clientCompany.save();
-
-    res.status(200).json({
-        success: true,
-        message: "Client deactivated",
-    });
-
+  res.status(200).json({
+    success: true,
+    message: "Client deactivated",
+  });
 };
 
 module.exports = {
-    getClients,
-    getClientById,
-    getLeadsCompany,
-    activateClient,
-    deactivateClient,
+  getClients,
+  getClientById,
+  getLeadsCompany,
+  activateClient,
+  deactivateClient,
 };
-
