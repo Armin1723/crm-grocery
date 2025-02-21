c8e8383c651aeb0d003cafb04f3b6ae4cdb632dc
Uzair Alam <a-8278@kmclu.ac.in>
2025-02-21 14:40:43 +0530

    fix:subscription cache reload

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -6,6 +6,7 @@ const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const { sendMail } = require("../helpers");
 const companyRegistrationMailTemplate = require("../templates/email/companyRegistrationMailTemplate");
+const { loadSubscriptionsToCache } = require("../helpers/subscriptionCache");
 
 const addCompany = async (req, res) => {
   if (
@@ -15,31 +16,46 @@ const addCompany = async (req, res) => {
     !req.body.phone ||
     !req.body.initials
   ) {
-    return res.status(400).json({ message: "All fields are required" , errors: {
-      name: "Company name is required",
-      address: "Company address is required",
-      email: "Company email is required",
-      phone: "Company phone is required",
-      initials: "Company initials are required"
-    }});
+    return res.status(400).json({
+      message: "All fields are required",
+      errors: {
+        name: "Company name is required",
+        address: "Company address is required",
+        email: "Company email is required",
+        phone: "Company phone is required",
+        initials: "Company initials are required",
+      },
+    });
   }
   const existingCompany = await Company.findOne({ name: req.body.name });
   if (existingCompany) {
-    return res.status(400).json({ success: false, message: "Company already exists", errors:{
-      name: "Company name already exists"
-    } });
+    return res.status(400).json({
+      success: false,
+      message: "Company already exists",
+      errors: {
+        name: "Company name already exists",
+      },
+    });
   }
   const existingInitials = await Company.findOne({
     initials: req.body.initials,
   });
   if (existingInitials) {
-    return res.status(400).json({ message: "Initials already exists", success: false, errors:{
-      initials: "Company initials already taken."
-    } });
+    return res.status(400).json({
+      message: "Initials already exists",
+      success: false,
+      errors: {
+        initials: "Company initials already taken.",
+      },
+    });
   }
 
   // Generate license key and trial end date
-  const licenseKey = 'CRM' + Math.random().toString(36).substr(2, 10).toUpperCase() + req.body.initials.toUpperCase() + new Date().getTime();
+  const licenseKey =
+    "CRM" +
+    Math.random().toString(36).substr(2, 10).toUpperCase() +
+    req.body.initials.toUpperCase() +
+    new Date().getTime();
   const subscriptionEndDate = new Date();
   subscriptionEndDate.setDate(subscriptionEndDate.getDate() + 7);
 
@@ -85,7 +101,7 @@ const addCompany = async (req, res) => {
     }
   }
 
-  await company.save(); 
+  await company.save();
 
   const generateUUID = async () => {
     let uuid =
@@ -99,34 +115,39 @@ const addCompany = async (req, res) => {
       return uuid;
     }
   };
-  
+
   // Update user with company id and initials
   const user = await User.findByIdAndUpdate(req.user.id);
   user.company = company._id;
-  if(!user.uuid){
+  if (!user.uuid) {
     user.uuid = await generateUUID();
   }
   await user.save();
 
-  const populatedUser = await User.findById(user._id).populate("company").lean();
+  const populatedUser = await User.findById(user._id)
+    .populate("company")
+    .lean();
 
   const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-        permissions: user.permissions,
-        company: populatedUser?.company?._id,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_EXPIRES_IN }
-    );
-    res.cookie("token", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-    });
-    user.password = undefined;
+    {
+      id: user._id,
+      role: user.role,
+      permissions: user.permissions,
+      company: populatedUser?.company?._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+  res.cookie("token", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  user.password = undefined;
+
+  // Reload cache
+  loadSubscriptionsToCache();
 
   res.status(201).json({
     message: "Company created successfully.",
@@ -138,12 +159,10 @@ const addCompany = async (req, res) => {
     await sendMail(
       user?.email,
       "Company Registration Successful",
-      companyRegistrationMailTemplate(
-        populatedUser, company
-      )
-    )
+      companyRegistrationMailTemplate(populatedUser, company)
+    );
   });
-}
+};
 
 const getCompanies = async (req, res) => {
   const { limit = 10, page = 1 } = req.query;
@@ -220,8 +239,12 @@ const updateCompany = async (req, res) => {
 
   await company.save();
 
-  const user = await User.findOne({ company: company._id }).populate("company").lean();
-  return res.status(200).json({ message: "Company updated successfully", user });
+  const user = await User.findOne({ company: company._id })
+    .populate("company")
+    .lean();
+  return res
+    .status(200)
+    .json({ message: "Company updated successfully", user });
 };
 
 const deleteCompany = async (req, res) => {
