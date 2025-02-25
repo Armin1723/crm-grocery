193dd9ce295183f28033f990c95b98562fee880c
Uzair Alam <a-8278@kmclu.ac.in>
2025-02-25 13:00:26 +0530

    refactor: improve code formatting and add rate limiting middleware

--- server/index.js
+++ server/index.js
@@ -1,49 +1,61 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const connectToDB = require('./db');
-const cookieParser = require('cookie-parser');
-const { errorHandler } = require('./middleware/errorHandler.js');
-const { loadSubscriptionsToCache } = require('./helpers/subscriptionCache.js');
-const dailySubscriptionsCacheLoadCron = require('./crons/dailySubscriptionsCacheLoadCron.js');
-const expiringProductsCron = require('./crons/expiringProductsCron.js');
+const express = require("express");
+const cors = require("cors");
+const connectToDB = require("./db");
+const cookieParser = require("cookie-parser");
+const { errorHandler } = require("./middleware/errorHandler.js");
+const { loadSubscriptionsToCache } = require("./helpers/subscriptionCache.js");
+const dailySubscriptionsCacheLoadCron = require("./crons/dailySubscriptionsCacheLoadCron.js");
+const expiringProductsCron = require("./crons/expiringProductsCron.js");
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 // Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));    
-app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
-    credentials: true
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
+
+app.use(
+  rateLimit({
+    windowMs: 1 * 60 * 1000, 
+    max: 30, 
+    message: "Too many requests, please try again later.",
+  })
+);
 
 // Database Connection
 connectToDB();
 
 // Routes
-app.use('/api/v1/products', require('./routes/product.routes.js'));
-app.use('/api/v1/purchases', require('./routes/purchase.routes.js'));
-app.use('/api/v1/suppliers', require('./routes/supplier.routes.js'));
-app.use('/api/v1/employees', require('./routes/employee.routes.js'));
-app.use('/api/v1/inventory', require('./routes/inventory.routes.js'));
-app.use('/api/v1/reports', require('./routes/report.routes.js'));
-app.use('/api/v1/sales', require('./routes/sale.routes.js'));
-app.use('/api/v1/stats', require('./routes/stat.routes.js'));
-app.use('/api/v1/auth', require('./routes/auth.routes.js'));
-app.use('/api/v1/expenses', require('./routes/expense.routes.js'));
-app.use('/api/v1/customers', require('./routes/customer.routes.js'));
-app.use('/api/v1/companies', require('./routes/company.routes.js'));
+app.use("/api/v1/products", require("./routes/product.routes.js"));
+app.use("/api/v1/purchases", require("./routes/purchase.routes.js"));
+app.use("/api/v1/suppliers", require("./routes/supplier.routes.js"));
+app.use("/api/v1/employees", require("./routes/employee.routes.js"));
+app.use("/api/v1/inventory", require("./routes/inventory.routes.js"));
+app.use("/api/v1/reports", require("./routes/report.routes.js"));
+app.use("/api/v1/sales", require("./routes/sale.routes.js"));
+app.use("/api/v1/stats", require("./routes/stat.routes.js"));
+app.use("/api/v1/auth", require("./routes/auth.routes.js"));
+app.use("/api/v1/expenses", require("./routes/expense.routes.js"));
+app.use("/api/v1/customers", require("./routes/customer.routes.js"));
+app.use("/api/v1/companies", require("./routes/company.routes.js"));
 
 // Support Panel Routes
-app.use('/api/v1/support', require('./routes/support.routes.js'));
+app.use("/api/v1/support", require("./routes/support.routes.js"));
 
 // Server
-app.get('/', (req, res) => {
-    res.send('Hello World from CRM API');
+app.get("/", (req, res) => {
+  res.send("Hello World from CRM API");
 });
 
 // Load Subscription Cache
@@ -57,5 +69,5 @@ expiringProductsCron();
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
